5a4e376432c6266daedd2c4cca6b3e119e7f8ecb
xzxgit <m13694555712@163.com>
2018-06-30 09:47:07 +0800

    [ADD] 自定义model的前台展示/搜索/过滤

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -5,7 +5,13 @@ from models import EmailVerifyRecord
 
 
 class EmailVerifyRecordAdmin(object):
-    pass
+    # 自定义xadmin端需要显示打列
+    list_display = ['code', 'email', 'type', 'send_time']
+    # 自定义搜索字段
+    search_fields = ['code', 'email', 'type']
+    # 自定义筛选字段
+    list_filter = ['code', 'email', 'type', 'send_time']
+
 
 # 注册邮件验证码
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
