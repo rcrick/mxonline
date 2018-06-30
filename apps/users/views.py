c0f5cfcafa0631c8e0c847df7b45c32c284ddbe6
xzxgit <m13694555712@163.com>
2018-06-30 23:05:28 +0800

    [UPDATE] 修改为cbv模式

--- apps/users/views.py
+++ apps/users/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
+from django.views.generic.base import View
 
 from models import UserProfile
 
@@ -10,14 +11,18 @@ from models import UserProfile
 class CustomBackend(ModelBackend):
     def authenticate(self, username=None, password=None, **kwargs):
         try:
-            user = UserProfile.objects.get(Q(username=username)|Q(email=username))
+            user = UserProfile.objects.get(Q(username=username) | Q(email=username))
             if user.check_password(password):
                 return user
         except Exception as e:
             return None
 
-def user_login(request):
-    if request.method == 'POST':
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "login.html", {})
+
+    def post(self, request):
         user_name = request.POST.get('username', '')
         pass_word = request.POST.get('password', '')
         user = authenticate(username=user_name, password=pass_word)
@@ -27,5 +32,3 @@ def user_login(request):
             return render(request, "index.html")
         else:
             return render(request, "login.html", {"message": u"用户名或密码错误!"})
-    elif request.method == 'GET':
-        return render(request, "login.html", {})
